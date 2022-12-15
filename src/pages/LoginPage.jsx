f88386c95ac3a533138fd2ffae8ec010844a8f16
Bakyt <bakyt.gaipov.kk@gmail.com>
2022-12-15 15:27:17 +0600

    first commit

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,62 +1,53 @@
-import React, { useState } from 'react'
-import '../styles/LoginPage.css'
-import { useNavigate } from 'react-router-dom'
-import { useAuth } from '../contexts/authContext'
-import { motion } from 'framer-motion'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './loginPage.css';
 
 const LoginPage = () => {
-	const { login } = useAuth()
-
-	const [username, setNickname] = useState('')
-	const [password, setPassword] = useState('')
-
-	const navigate = useNavigate()
-
-	function loginSystem() {
-		if (!username.trim() || !password.trim()) {
-			alert('Some inputs are empty')
-			return
-		}
-		login(username, password)
-		navigate('/')
-	}
-
+	
 	return (
-		<motion.div
-			className='login-page'
-			initial={{ width: 0, opacity: 0 }}
-			animate={{ width: '80vw', opacity: 1 }}
-			exit={{ width: window.innerWidth, opacity: 0 }}
-		>
-			<div className='login-form'>
-				<h3>Sign In</h3>
-				<input
-					type='text'
-					value={username}
-					onChange={e => setNickname(e.target.value)}
-					placeholder='Nickname'
-					className='login_nickname-inp'
-				/>
-				<input
-					value={password}
-					onChange={e => setPassword(e.target.value)}
-					type='text'
-					placeholder='Password'
-					className='login_password-inp'
-				/>
-				<h6 onClick={() => navigate('/recovery')} className='recovery'>
-					Forgot your Password?
-				</h6>
-				<h5>Don't have an account?</h5>
-				<h6 onClick={() => navigate('/register')} className='reg-link'>
-					Register?
-				</h6>
-				<button className='login-btn' onClick={loginSystem}>
-					Sign In
-				</button>
-			</div>
-		</motion.div>
+		<main>
+			<section className="login-container">
+				<h3 className='login-title'>Войти</h3>
+				<form className="login-form">
+					<label>
+						Имя
+					</label>
+					<p>
+						<input 
+							type="username" 			
+							className="login-username" 
+						/>
+					</p>
+					
+					<label>
+						Пароль
+					</label>
+					<p>
+						<input 
+							type="password"
+							className="login-password" 
+						/>
+					</p>
+				</form>
+					
+				<div className="forgot-pass">
+					<Link to="/forgotpassword" className="forgot-pass-link">
+						Забыли пароль?
+					</Link>
+				</div>
+				<div className="login-buttons">
+					<button className="login-btn">
+						Войти
+					</button>
+					<div className="registration-btn">
+						<Link className='registration-link' to='/registration'>
+							Регистрация
+						</Link>
+					</div>
+				</div>
+			</section>
+		</main>
 	)
 }
 
-export default LoginPage
+export default LoginPage;
